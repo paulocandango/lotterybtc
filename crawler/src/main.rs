04a847b794b1759bb44178a22ba04e2a1618008c
Paulo.Moreira <paulocandango@gmail.com>
2024-10-31 19:15:37 -0300

    Criando a estrutura Rust

---
+++ crawler/src/main.rs
@@ -0,0 +1,7 @@
+use shared::{example_function};
+
+fn main() {
+    println!("Crawler Rodando!");
+
+    shared::example_function();
+}
---
+++ shared/src/lib.rs
@@ -0,0 +1,4 @@
+// Exemplo de função pública que poderá ser usada por `crawler` e `server`
+pub fn example_function() {
+    println!("Essa é uma função compartilhada!");
+}
