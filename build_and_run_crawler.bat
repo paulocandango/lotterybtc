e00a2d1df26c8bd43a0edf5a39685ef36aa3a8b0
Paulo.Moreira <paulocandango@gmail.com>
2024-11-02 10:47:23 -0300

    Começando a brincadeira

--- build_and_run_crawler.bat
+++ build_and_run_crawler.bat
@@ -1,2 +1,8 @@
+cd C:\dev\lotterybtc
 podman build -t crawler-prod -f crawler/Dockerfile .
 podman run crawler-prod
+
+Exportar e Importar e Executar
+podman save -o crawler-prod.tar localhost/crawler-prod:latest
+podman load -i crawler-prod.tar
+podman run -it --rm crawler-prod
